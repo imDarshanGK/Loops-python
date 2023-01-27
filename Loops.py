6482f592aa52d4771ef72762ef7b6659f602dc95
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-01-27 22:21:19 +0530

    Loop
    It is used to execute a statement or Group of statement for multiple time.

---
+++ Loops.py
@@ -0,0 +1,6 @@
+#It is used to execute a statement or Group of statement for multiple time.
+i=80
+while(i<=100):
+    print("%d\n",i)
+    i=i+1
+print("loop%d\n",i)
---
+++ loop.py
@@ -0,0 +1,6 @@
+i=1
+while(i<=100):
+    if(i%7==0 and i%3==0):
+        print(i)
+    i=i+1
+print("Loops\n",i)
