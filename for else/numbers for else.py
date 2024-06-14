0326ebea2935a3fddffa3d2041e2de684c4097a9
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-06-14 18:46:00 +0530

    numbers

---
+++ for else/numbers for else.py
@@ -0,0 +1,8 @@
+num = (-1,43,4,5,7,8,9,23,53,7)
+for numbers in num:
+    print(numbers)
+    if numbers==5:
+        break
+else:
+    print("Loop successfully completed and we are in else block now!!")
+print("Out of for else")    
