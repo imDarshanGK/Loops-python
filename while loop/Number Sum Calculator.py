b86339a3a80ad80b3c0f969206fe2193921ee3fd
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-07-12 23:25:09 +0530

    Number Sum Calculator

---
+++ while loop/Number Sum Calculator.py
@@ -0,0 +1,8 @@
+total = 0
+number = int(input("Enter a number(0 to quit)>> "))
+while number!=0:
+    total = total+number
+    number = int(input("Enter a number(0 to quit)>> "))
+print("Total is",total)
+    
+    
